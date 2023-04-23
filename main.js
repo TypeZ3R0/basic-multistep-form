b53669ffe3deab7121b9b0d6d4025a065280e417
Aranyak Samui <aranyaksamuiarya@gmail.com>
2023-04-23 22:15:41 +0530

    Add all three divs and navigation between them

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 // Getting all the HTML elements
+// Form divs
 const first = document.querySelector("#first");
 const second = document.querySelector("#second");
+const last = document.querySelector("#last");
+
+// Navigation buttons
 const nextBtnOne = document.querySelector("#next-1");
 const prevBtnOne = document.querySelector("#prev-1");
 const nextBtnTwo = document.querySelector("#next-2");
+const prevBtnTwo = document.querySelector("#prev-2");
+
+// Form internals
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+
+
 
 // Hide first div and show second div
 nextBtnOne.addEventListener("click", () => {
@@ -16,3 +27,14 @@ prevBtnOne.addEventListener("click", () => {
     first.style.display = "flex";
     second.style.display = "none";
 });
+// Hide second div and show last div
+nextBtnTwo.addEventListener("click", () => {
+    second.style.display = "none";
+    last.style.display = "flex";
+});
+
+// Hide last div and show second div
+prevBtnTwo.addEventListener("click", () => {
+    last.style.display = "none";
+    second.style.display = "flex";
+})
